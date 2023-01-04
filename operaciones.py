12c014bc192f93459736f2b9ce50871a28e33698
SilvinaCantello <102930584+SilvinaCantello@users.noreply.github.com>
2023-01-03 22:30:08 -0300

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,213 @@
+
+def calcular(a,b,c,d):
+    if a+b+c+d==10:
+        print(f"{a}+{b}+{c}+{d}=10")
+        return 1    
+    elif a+b+c-d==10:
+        print( f"{a}+{b}+{c}-{d}=10")
+        return 2
+    elif a+b-c+d==10:
+        print(f"{a}+{b}-{c}+{d}=10")
+        return 3
+    elif a-b+c+d==10:
+        print(f"{a}-{b}+{c}+{d}=10")
+        return 4
+    elif a+b+c*d==10:
+        print(f"{a}+{b}+{c}*{d}=10")
+        return 5
+    elif a+b*c+d==10:
+        print(f"{a}+{b}*{c}+{d}=10")
+        return 6
+    elif a*b+c+d==10:
+        print(f"{a}*{b}+{c}+{d}=10")
+        return 7
+    elif a+b+c/d==10:
+        print(f"{a}+{b}+{c}/{d}=10")
+        return 8
+    elif a+b/c+d==10:
+        print(f"{a}+{b}/{c}+{d}=10")
+        return 9
+    elif a/b+c+d==10:
+        print(f"{a}/{b}+{c}+{d}=10")
+        return 10
+    elif a-b-c-d==10:
+        print( f"{a}-{b}-{c}-{d}=10")
+        return 11
+    elif a-b+c-d==10:
+        print(f"{a}-{b}+{c}-{d}=10")
+        return 12
+    elif a+b-c-d==10:
+        print(f"{a}+{b}-{c}-{d}=10")
+        return 13
+    elif a-b-c+d==10:
+        print(f"{a}-{b}-{c}+{d}=10")
+        return 14
+    elif a-b-c*d==10:
+        print(f"{a}-{b}-{c}*{d}=10")
+        return 15
+    elif a-b*c-d==10:
+        print(f"{a}-{b}*{c}-{d}=10")
+        return 16
+    elif a*b-c-d==10:
+        print( f"{a}*{b}-{c}-{d}=10")
+        return 17
+    elif a-b-c/d==10:
+        print(f"{a}-{b}-{c}/{d}=10")
+        return 18
+    elif a-b/c-d==10:
+        print(f"{a}-{b}/{c}-{d}=10")
+        return 19
+    elif a/b-c-d==10:
+        print(f"{a}/{b}-{c}-{d}=10")
+        return 20
+    elif a*b*c*d==10:
+        print(f"{a}*{b}*{c}*{d}=10")
+        return 21
+    elif a*b*c+d==10:
+        print(f"{a}*{b}*{c}+{d}=10")
+        return 22
+    elif a*b+c*d==10:
+        print(f"{a}*{b}+{c}*{d}=10")
+        return 23
+    elif a+b*c*d==10:
+        print(f"{a}+{b}*{c}*{d}=10")
+        return 24
+    elif a*b*c-d==10:
+        print(f"{a}*{b}*{c}-{d}=10")
+        return 25
+    elif a*b-c*d==10:
+        print(f"{a}*{b}-{c}*{d}=10")
+        return 26
+    elif a-b*c*d==10:
+        print(f"{a}-{b}*{c}*{d}=10")
+        return 27
+    elif a*b*c/d==10:
+        print(f"{a}*{b}*{c}/{d}=10")
+        return 28
+    elif a*b/c*d==10:
+        print(f"{a}*{b}/{c}*{d}=10")
+        return 29
+    elif a/b*c*d==10:
+        print(f"{a}/{b}*{c}*{d}=10")
+        return 30
+    elif a/b/c/d==10:
+        print(f"{a}/{b}/{c}/{d}=10")
+        return 31
+    elif a/b/c+d==10:
+        print(f"{a}/{b}/{c}+{d}=10")
+        return 32
+    elif a/b+c/d==10:
+        print(f"{a}/{b}+{c}/{d}=10")
+        return 33
+    elif a+b/c/d==10:
+        print(f"{a}+{b}/{c}/{d}=10")
+        return 34
+    elif a/b/c-d==10:
+        print(f"{a}/{b}/{c}-{d}=10")
+        return 35
+    elif a/b-c/d==10:
+        print(f"{a}/{b}-{c}/{d}=10")
+        return 36
+    elif a-b/c/d==10:
+        print( f"{a}-{b}/{c}/{d}=10")
+        return 37
+    elif a/b/c*d==10:
+        print(f"{a}/{b}/{c}*{d}=10")
+        return 38
+    elif a/b*c/d==10:
+        print(f"{a}/{b}*{c}/{d}=10")
+        return 39
+    elif a*b/c/d==10:
+        print(f"{a}*{b}/{c}/{d}=10")
+        return 40
+    elif a+b*c/d==10:
+        print(f"{a}+{b}*{c}/{d}=10")
+        return 41
+    elif a+b/c*d==10:
+        print(f"{a}+{b}/{c}*{d}=10")
+        return 42
+    elif a+b*c-d==10:
+        print(f"{a}+{b}*{c}-{d}=10")
+        return 43
+    elif a+b-c*d==10:
+        print(f"{a}+{b}-{c}*{d}=10")
+        return 44
+    elif a-b*c/d==10:
+        print(f"{a}-{b}*{c}/{d}=10")
+        return 45
+    elif a-b/c*d==10:
+        print(f"{a}-{b}/{c}*{d}=10")
+        return 46
+    elif a-b*c+d==10:
+        print(f"{a}-{b}*{c}+{d}=10")
+        return 47
+    elif a-b+c*d==10:
+        print(f"{a}-{b}+{c}*{d}=10")
+        return 48
+    elif a*b+c/d==10:
+        print(f"{a}*{b}+{c}/{d}=10")
+        return 49
+    elif a*b/c+d==10:
+        print(f"{a}*{b}/{c}+{d}=10")
+        return 50
+    elif a*b-c/d==10:
+        print(f"{a}*{b}-{c}/{d}=10")
+        return 51
+    elif a*b/c-d==10:
+        print(f"{a}*{b}/{c}-{d}=10")
+        return 52
+    elif a/b+c-d==10:
+        print(f"{a}/{b}+{c}-{d}=10")
+        return 53
+    elif a/b-c+d==10:
+        print(f"{a}/{b}-{c}+{d}=10")
+        return 54
+    elif a/b*c-d==10:
+        print(f"{a}/{b}*{c}-{d}=10")
+        return 55
+    elif a/b-c*d==10:
+        print(f"{a}/{b}-{c}*{d}=10")
+        return 56
+    elif a+b-c/d==10:
+        print(f"{a}+{b}-{c}/{d}=10")
+        return 57
+    elif a+b/c-d==10:
+        print(f"{a}+{b}/{c}-{d}=10")
+        return 58
+    
+    elif a-b+c/d==10:
+        print(f"{a}-{b}+{c}/{d}=10")
+        return 59
+    elif a-b/c+d==10:
+        print(f"{a}-{b}/{c}+{d}=10")
+        return 60
+    elif a*b-c+d==10:
+        print(f"{a}*{b}-{c}+{d}=10")
+        return 61
+    elif a*b+c-d==10:
+        print(a, "*",b, "+", c, "-", d, "= 10" )
+        return 62
+    elif a/b*c+d==10:
+        print(f"{a}/{b}*{c}+{d}=10")
+        return 63
+    elif a/b+c*d==10:
+        print(f"{a}/{b}+{c}*{d}=10")
+        return 64
+    else:
+        print("No existe ninguna operacion.")
+
+def main():
+    while True:
+        try:
+            a= int(input("Ingrese el primer digito: "))
+            b= int(input("Ingrese el segundo digito: "))
+            c= int(input("Ingrese el tercer digito: "))
+            d= int(input("Ingrese el cuarto digito: "))
+            break
+            
+        except:
+            print("Error: No se ha ingresado un valor numerico. \n Intentelo nuevamente.")
+
+    calculo= calcular(a,b,c,d)
+    
+main()
